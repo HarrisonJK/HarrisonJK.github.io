71a4c04d5372c7a96d9f9d1da50ab095e32f3470
Harrison J. Katapodis (Harrison J.K.) <hjk72@cornell.edu>
2023-04-12 16:34:25 -0400

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,47 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { About } from "./components/About";
+import { Portfolio } from "./components/Portfolio";
+import { Resume } from "./components/Resume";
+import { Contact } from "./components/Contact";
+import { Link } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        <Route path="/Resume" element={<Resume />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </div>
+    // <div className="App">
+    //   <div>
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://reactjs.org" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </div>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li className="test">
+          <img src="../../public/HKlogo.svg" />{" "}
+        </li>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <li>
+          <Link to="/Portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/Resume">Resume</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+// export default Navbar;
