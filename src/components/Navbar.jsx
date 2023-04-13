40f703b35ad9be95099e213db1f50e9787397857
Harrison J. Katapodis (Harrison J.K.) <hjk72@cornell.edu>
2023-04-13 13:35:28 -0400

    html structure navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,30 @@ import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li className="test">
+    <div class="entireNavbar">
+      <div class="container">
+        <nav class="navbarSite">
           <img src="HKLogo.svg" id="navLogo" />
-        </li>
-        <li>
-          <Link to="/">About</Link>
-        </li>
-        <li>
-          <Link to="/Portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link to="/Resume">Resume</Link>
-        </li>
-        <li>
-          <Link to="/Contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+          <ul>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/Portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link to="/Resume">Resume</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+          {/* <button class="mobileHamburger">
+            <span></span>
+          </button> */}
+        </nav>
+      </div>
+    </div>
   );
 }
 
