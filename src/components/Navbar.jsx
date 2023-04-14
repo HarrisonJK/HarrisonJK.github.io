222c837c9f144c3d3f984fe4ed6603815ca4517c
Harrison J. Katapodis (Harrison J.K.) <hjk72@cornell.edu>
2023-04-13 23:04:29 -0400

    Navbar straightened

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,28 @@ import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <div class="entireNavbar">
-      <div class="container">
-        <nav class="navbarSite">
-          <img src="HKLogo.svg" id="navLogo" />
-          <ul class="navbarTabs">
-            <li>
-              <Link to="/">About</Link>
-            </li>
-            <li>
-              <Link to="/Portfolio">Portfolio</Link>
-            </li>
-            <li>
-              <Link to="/Resume">Resume</Link>
-            </li>
-            <li>
-              <Link to="/Contact">Contact</Link>
-            </li>
-          </ul>
-          {/* <button class="mobileHamburger">
+    <nav class="navbarContainer">
+      <div class="logoContainer">
+        <img src="HKLogo.svg" id="navLogo" />
+      </div>
+      <ul class="navbarTabs">
+        <li class="flatten">
+          <Link to="/">About</Link>
+        </li>
+        <li class="flatten">
+          <Link to="/Portfolio">Portfolio</Link>
+        </li>
+        <li class="flatten">
+          <Link to="/Resume">Resume</Link>
+        </li>
+        <li class="flatten">
+          <Link to="/Contact">Contact</Link>
+        </li>
+      </ul>
+      {/* <button class="mobileHamburger">
             <span></span>
           </button> */}
-        </nav>
-      </div>
-    </div>
+    </nav>
   );
 }
 
