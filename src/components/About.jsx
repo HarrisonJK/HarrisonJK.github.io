c05803a8daac72af3122d49c233df78fd8b0379f
Harrison J. Katapodis (Harrison J.K.) <hjk72@cornell.edu>
2023-04-15 22:36:48 -0400

    remove boilerplate/cleaned up connecting code/ changed background color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,87 @@
 export function About() {
-  return <h1>About </h1>;
+  return (
+    <div className="aboutBody">
+      <section>
+        <h1>'Nice Curves'</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, delectus dicta minima beatae accusantium veritatis ab
+          eveniet perspiciatis praesentium modi, nam voluptatem, quos ullam ex
+          numquam. Ut quis blanditiis suscipit.
+        </p>
+      </section>
+      <section>
+        <h1>'Nice Curves'</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, delectus dicta minima beatae accusantium veritatis ab
+          eveniet perspiciatis praesentium modi, nam voluptatem, quos ullam ex
+          numquam. Ut quis blanditiis suscipit.
+        </p>
+      </section>
+      <section>
+        <h1>'Nice Curves'</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, delectus dicta minima beatae accusantium veritatis ab
+          eveniet perspiciatis praesentium modi, nam voluptatem, quos ullam ex
+          numquam. Ut quis blanditiis suscipit.
+        </p>
+      </section>
+      <section>
+        <h1>'Nice Curves'</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, delectus dicta minima beatae accusantium veritatis ab
+          eveniet perspiciatis praesentium modi, nam voluptatem, quos ullam ex
+          numquam. Ut quis blanditiis suscipit.
+        </p>
+      </section>
+      <section>
+        <h1>'Nice Curves'</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, delectus dicta minima beatae accusantium veritatis ab
+          eveniet perspiciatis praesentium modi, nam voluptatem, quos ullam ex
+          numquam. Ut quis blanditiis suscipit.
+        </p>
+      </section>
+      <section>
+        <h1>'Nice Curves'</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, delectus dicta minima beatae accusantium veritatis ab
+          eveniet perspiciatis praesentium modi, nam voluptatem, quos ullam ex
+          numquam. Ut quis blanditiis suscipit.
+        </p>
+      </section>
+      <section>
+        <h1>'Nice Curves'</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, delectus dicta minima beatae accusantium veritatis ab
+          eveniet perspiciatis praesentium modi, nam voluptatem, quos ullam ex
+          numquam. Ut quis blanditiis suscipit.
+        </p>
+      </section>
+      <section>
+        <h1>'Nice Curves'</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, delectus dicta minima beatae accusantium veritatis ab
+          eveniet perspiciatis praesentium modi, nam voluptatem, quos ullam ex
+          numquam. Ut quis blanditiis suscipit.
+        </p>
+      </section>
+      <section>
+        <h1>'Nice Curves'</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, delectus dicta minima beatae accusantium veritatis ab
+          eveniet perspiciatis praesentium modi, nam voluptatem, quos ullam ex
+          numquam. Ut quis blanditiis suscipit.
+        </p>
+      </section>
+    </div>
+  );
 }
