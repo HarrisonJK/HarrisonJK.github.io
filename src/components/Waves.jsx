a5889b85d7374a44880218a55da632f0b54ae652
Harrison J. Katapodis (Harrison J.K.) <hjk72@cornell.edu>
2023-04-19 23:11:07 -0400

    swapped code form about page to main page

--- src/components/Waves.jsx
+++ src/components/Waves.jsx
@@ -1,3 +1,6 @@
+import kefaWaves1 from "../assets/kefaWaves1.svg";
+import kefaWaves2 from "../assets/kefaWaves2.svg";
+
 export function Waves() {
   return <div className="spacer kefaWaves"></div>;
 }
