afeff54730db30b629389012115f093fb60f25d2
Harrison J. Katapodis (Harrison J.K.) <hjk72@cornell.edu>
2023-05-11 21:30:02 -0400

    added info to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 export function About() {
-  function RandomButton() {
+  function WikipediaButton() {
     const links = [
       "https://en.wikipedia.org/wiki/Graphical_user_interface",
       "https://en.wikipedia.org/wiki/Extreme_programming",
@@ -506,6 +508,7 @@ export function About() {
 
     return (
       <div onClick={handleClick} className="center">
+        →
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="48"
@@ -516,13 +519,14 @@ export function About() {
           <path
             fill="#CFD8DC"
             d="M6 10a4 4 0 0 1 4-4h28a4 4 0 0 1 4 4v28a4 4 0 0 1-4 4H10a4 4 0 0 1-4-4V10z"
-            className="test"
+            className="colorChange"
           />
           <path
             fill="#37474F"
             d="M39 17.271a.342.342 0 0 1-.334.349h-1.799l-8.164 18.179c-.052.12-.17.2-.297.202h-.004a.33.33 0 0 1-.298-.193l-3.874-8.039l-4.18 8.049a.333.333 0 0 1-.303.184a.336.336 0 0 1-.292-.199l-8.252-18.182h-1.87a.345.345 0 0 1-.333-.35v-.921a.34.34 0 0 1 .333-.35h6.657a.34.34 0 0 1 .333.35v.921a.342.342 0 0 1-.333.349h-1.433l5.696 13.748l2.964-5.793l-3.757-7.953h-.904a.342.342 0 0 1-.333-.35v-.922c0-.191.149-.348.333-.348h4.924a.34.34 0 0 1 .333.348v.922c0 .192-.149.35-.333.35h-.867l2.162 4.948l2.572-4.948H25.77a.34.34 0 0 1-.334-.35v-.922a.34.34 0 0 1 .334-.348h4.784c.187 0 .333.156.333.348v.922a.34.34 0 0 1-.333.35h-1.05l-3.757 7.141l3.063 6.584l5.905-13.725h-1.872a.343.343 0 0 1-.334-.35v-.922c0-.191.15-.348.334-.348h5.822a.34.34 0 0 1 .334.348v.921z"
           />
         </svg>
+        ←
       </div>
     );
   }
@@ -531,21 +535,32 @@ export function About() {
     <section className="aboutMe rainbowContainer">
       <h1 className="center montserrat">Hello!</h1>
       <p className="montserrat">
-        My name is Harrison Katapodis and I'm a software engineer! <br></br>I
-        started out in Finance, but during the pandemic realized I wanted a more
-        tangible skill that would allow me to be more creative. <br></br>I
-        created this website to showcase my experiences and give you insight
-        into who I am!
+        My name is Harrison Katapodis and I'm a software engineer! <br></br>
+        Check out some of of my favorite projects{" "}
+        <Link to="/portfolio">here!</Link> <br></br> I started out in Finance,
+        but during the pandemic realized I wanted a more tangible skill that
+        would allow me to be more creative. <br></br>I created this website to
+        showcase my experiences and give you insight into who I am!
         <h1 className="noMargin">
           The <mark>highlights</mark>
         </h1>
         <li className="space">
           Graduate of Cornell University and Fullstack Academy
         </li>
-        <li>I am a lifelong learner who regularly wikipedia binges</li>
+        <li>PERN Stack Educated and learning Python</li>
+        <li>
+          Passionate about helping others and making the world a better place
+        </li>
+        <li>
+          Lifelong learner always trying to be better than i was the day before
+        </li>
       </p>
-      <icon icon="mdi-light:home"></icon>
-      <RandomButton />
+      <div className="montserrat center botPadding">
+        Wikipedia button (Some of my favorites!)
+      </div>
+      <div>
+        <WikipediaButton />
+      </div>
     </section>
   );
 }
